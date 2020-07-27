56402e27b513e23cafad67adde57773d23f9fbf7
Maria Mihai <maria.ic.mihai@gmail.com>
2020-07-27 18:45:02 +0200

    Initial commit #1

---
+++ src/main/java/guru/springframework/sbmconfigserver/SbmConfigServerApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sbmconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbmConfigServerApplication.class, args);
+    }
+
+}
